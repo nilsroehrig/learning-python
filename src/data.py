ebd73384d2cf4968754e84c6557d82aa5c3c8c2a
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-10-29 00:05:19 +0200

    data types

---
+++ src/data.py
@@ -0,0 +1,34 @@
+a = 5
+print(type(a))
+b = "5"
+print(type(b))
+c = True
+d = False
+print(type(c), type(d))
+print(5 > 10)
+
+# List Type
+test_list = [50, "100", True, 50, "50"]
+
+print(test_list)
+print(test_list[1])
+print(len(test_list))
+print(type(test_list))
+
+# Tuple Type
+test_tuple = (50, "100", True, 50, "50")
+print(test_tuple)
+print(len(test_tuple))
+print(type(test_tuple))
+
+# Set Type
+test_set = {50, "100", True, 50, "50", 50, 100, 100, "a"}
+print(test_set)
+print(len(test_set))
+print(type(test_set))
+
+# Dict Type
+test_dict = {"first": 50, "second": "100", "third": True, "a": 50, "a": "LAST"}
+print(test_dict)
+print(len(test_dict))
+print(type(test_dict))
