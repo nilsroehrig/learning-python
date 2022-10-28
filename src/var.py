b724ff8b8b55249c64dd95a16ae6fc03e463153f
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-10-28 23:46:46 +0200

    variables

---
+++ src/var.py
@@ -0,0 +1,39 @@
+# start with letter or _, cannot start with number
+# a-Z0-9_
+a, b, c = 5, 10, 15
+
+print(c)
+
+d = e = f = 50
+
+print(d, e, f)
+data = ["A", "B", "C"]
+
+g, h, i = data
+
+print(g, h, i)
+
+a = 5
+b = 10
+a = 55
+
+message = "Hello World"
+
+print(a + b)
+print(5 + 10)
+print(message)
+
+a = str(a)
+print(type(a))
+a = int(a)
+print(type(a))
+print(a)
+
+str1 = "hello world"
+str2 = 'hello'
+str3 = "hello' world"
+
+Str1 = "Hello"
+
+print(str1)
+print(Str1)
