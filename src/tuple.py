ba1af0cb9c373085520e683a1b07d485acf28409
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-10-29 12:34:39 +0200

    tuples

---
+++ src/tuple.py
@@ -0,0 +1,7 @@
+test_tuple = ("Hello", "Laurence", "Svekis", 100, True, "Svekis", "Svekis")
+length = len(test_tuple)
+print(test_tuple)
+print(length)
+svekis = test_tuple[2]
+print(svekis)
+# test_tuple[2] = "FOO" # not allowed
