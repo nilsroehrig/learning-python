dbd02a580447ebed999c2afd44e96d68370e3be9
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-10-29 13:01:33 +0200

    built ins

---
+++ src/temp.py
@@ -0,0 +1,12 @@
+print(5 * 8)
+minimum = min(4, 6, 4354, 345, 23, 1, 54, 6, 433, 6665, 3434)
+print(minimum)
+
+maximum = max(4, 6, 4354, 345, 23, 1, 54, 6, 433, 6665, 3434)
+print(maximum)
+
+rounding = round(4.6)
+print(rounding)
+
+rounding2 = round(4.4)
+print(rounding2)
