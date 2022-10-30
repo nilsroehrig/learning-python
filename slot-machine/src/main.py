f43c065a520f81937561efcc87650ee66f824f70
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-10-30 19:29:26 +0100

    wrap up slot machine

--- slot-machine/src/main.py
+++ slot-machine/src/main.py
@@ -14,6 +14,29 @@ symbol_count = {
     "D": 6
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
@@ -88,6 +111,16 @@ def get_bet():
 
 def main():
     balance = deposit()
+    while True:
+        print(f"Current balance is: ${balance}.")
+        answer = input("Press enter to play (q to quit.) ")
+        if answer == 'q':
+            break
+        balance += spin(balance)
+    print(f"You left with ${balance}.")
+
+
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -97,11 +130,13 @@ def main():
             print(f"You cannot bet ${total_bet}, because it exceeds your balance of ${balance}.")
         else:
             break
-
     print(f"You are betting ${bet} on {lines} lines. Total bet is ${bet * lines}.")
-
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}.")
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
 
 
 main()
