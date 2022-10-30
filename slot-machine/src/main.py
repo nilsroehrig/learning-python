397b50212e6dfff78bcdc5f4e0f059b452482bd5
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-10-30 18:21:39 +0100

    add number of lines

---
+++ slot-machine/src/main.py
@@ -0,0 +1,38 @@
+MAX_LINES = 3
+
+
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $: ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+    return amount
+
+
+def get_number_of_lines():
+    while True:
+        lines = input(f"Enter the number of lines to bet on (1-{MAX_LINES}). $: ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print(f"Number of lines must be between 1 and {MAX_LINES}.")
+        else:
+            print("Please enter a number.")
+    return lines
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+
+main()
