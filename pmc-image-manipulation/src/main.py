223789434533dd9e125aee2383e086d91c0c48e3
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-11-06 22:13:36 +0100

    intro to cv2

---
+++ pmc-image-manipulation/src/main.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread("../materials/galaxy.jpg", 0)
+
+print(img)
+print(type(img))
+print(img.shape)
+print(img.ndim)
+
+resized_image=cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+cv2.imwrite("../materials/Galaxy_resized.jpg", resized_image)
+cv2.imshow("Galaxy", resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
