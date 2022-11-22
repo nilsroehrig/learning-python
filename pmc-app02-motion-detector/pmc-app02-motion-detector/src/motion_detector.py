f4e32942ed4b8cd90b4c34f1a260b2879f085245
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-11-22 17:43:28 +0100

    video capture app part 2

--- pmc-app02-motion-detector/pmc-app02-motion-detector/src/motion_detector.py
+++ pmc-app02-motion-detector/pmc-app02-motion-detector/src/motion_detector.py
@@ -1,8 +1,13 @@
+from datetime import datetime
 import cv2
+import pandas
 
 video = cv2.VideoCapture(0)
 
 first_frame = None
+motion_detected = False
+latest_motion_detection_start = None
+timespans = []
 
 while True:
     check, current_frame = video.read()
@@ -20,21 +25,32 @@ while True:
 
     contours, _ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    if not motion_detected and len(contours) > 0:
+        motion_detected = True
+        latest_motion_detection_start = datetime.now()
+
+    if motion_detected and len(contours) == 0:
+        motion_detected = False
+        timespans.append((latest_motion_detection_start, datetime.now()))
+
     for contour in contours:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(current_frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-    # cv2.imshow("Capture", current_frame_gray)
-    # cv2.imshow("Delta Frame", delta_frame)
-    # cv2.imshow("Thresholded Delta Frame", thresh_frame)
-    # cv2.imshow("Countours", contours)
-
     cv2.imshow("Color", current_frame)
 
     if cv2.waitKey(1) == ord("q"):
+        if motion_detected:
+            timespans.append((latest_motion_detection_start, datetime.now()))
         break
+
+timespan_dataframe = pandas.DataFrame()
+timespan_dataframe["start"] = [start for start, _ in timespans]
+timespan_dataframe["end"] = [end for _, end in timespans]
+
+timespan_dataframe.to_csv("Times.csv")
 
 video.release()
 cv2.destroyAllWindows()
