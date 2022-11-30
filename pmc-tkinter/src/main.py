ffd9c79ff80895a12de517e230fcb93734e8c2b9
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-11-30 16:28:47 +0100

    tkinter commands

--- pmc-tkinter/src/main.py
+++ pmc-tkinter/src/main.py
@@ -1,11 +1,20 @@
 from tkinter import *
 
+
+def km_to_miles():
+    # print(entry1_value.get())
+    miles = float(entry1_value.get()) * 1.6
+    text1.insert(END, str(miles))
+
+
 window = Tk()
 
-btn1 = Button(window, text="Execute")
+btn1 = Button(window, text="Execute", command=km_to_miles)
 btn1.grid(row=0, column=0)
 
-entry1 = Entry(window)
+entry1_value = StringVar()
+
+entry1 = Entry(window, textvariable=entry1_value)
 entry1.grid(row=0, column=1)
 
 text1 = Text(window, height=1, width=20)
