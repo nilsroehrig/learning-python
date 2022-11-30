f6a26d1ea4826dcaecddaf55494fe2dcae050562
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-11-30 14:00:18 +0100

    add template rendering

--- pmc-app04-flask-website/src/main.py
+++ pmc-app04-flask-website/src/main.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello Flask!"
+    return render_template("home.html")
 
 
 @app.route('/about/')
 def about():
-    return "Stuff about Flask!"
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
