7bff35bd6435c192ff98bec0e1cb360d33cfb7fc
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-12-02 22:57:54 +0100

    add first operations for book store

---
+++ pmc-app05-bookstore/src/book_operations.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+def with_db(fn):
+    conn = sqlite3.connect("book_store.sqlite")
+    result = fn(conn.cursor())
+    conn.commit()
+    conn.close()
+    return result
+
+
+def init_database():
+    return with_db(lambda cur: cur.execute(
+        'CREATE TABLE IF NOT EXISTS books (title TEXT, author TEXT, year INTEGER, isbn INTEGER)'))
+
+
+def create_entry(title, author, year, isbn):
+    book_attributes = (title, author, year, isbn)
+    if any(val is None for val in book_attributes):
+        raise Exception("All book attributes must be set!")
+
+    return with_db(lambda cur: cur.execute('INSERT INTO books VALUES (?, ?, ?, ?)', book_attributes))
+
+
+# def get_by_title(title):
+#     return with_db(lambda cur: cur.execute("SELECT * FROM books WHERE title=?", (title,)))
+
+
+init_database()
