42f401be1143c6643ba4e20c9da36785b34ad255
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-12-02 23:12:49 +0100

    set more sensible table constraints, add remove action

--- pmc-app05-bookstore/src/book_operations.py
+++ pmc-app05-bookstore/src/book_operations.py
@@ -11,28 +11,24 @@ def with_db(fn):
 
 def init_database():
     return with_db(lambda cur: cur.execute(
-        'CREATE TABLE IF NOT EXISTS books (title TEXT, author TEXT, year INTEGER, isbn INTEGER)'))
+        'CREATE TABLE IF NOT EXISTS books (title TEXT NOT NULL, author TEXT NOT NULL, year INTEGER NOT NULL, isbn INTEGER PRIMARY KEY)'))
 
 
 def create_book(title, author, year, isbn):
-    book_attributes = (title, author, year, isbn)
-    if any(val is None for val in book_attributes):
-        raise Exception("All book attributes must be set!")
-
-    return with_db(lambda cur: cur.execute('INSERT INTO books VALUES (?, ?, ?, ?)', book_attributes))
+    return with_db(lambda cur: cur.execute('INSERT INTO books VALUES (?, ?, ?, ?)', (title, author, year, isbn)))
 
 
 def update_book(title, author, year, isbn):
-    book_attributes = (title, author, year, isbn)
-    if any(val is None for val in book_attributes):
-        raise Exception("All book attributes must be set!")
+    return with_db(
+        lambda cur: cur.execute("UPDATE books SET title=?, author=?, year=? WHERE isbn=?", (title, author, year, isbn)))
 
-    return with_db(lambda cur: cur.execute("UPDATE books SET title=?, author=?, year=? WHERE isbn=?", book_attributes))
+
+def remove_book(isbn):
+    return with_db(lambda cur: cur.execute("DELETE FROM books WHERE isbn=?", (isbn,)))
 
 
 # def get_by_title(title):
 #     return with_db(lambda cur: cur.execute("SELECT * FROM books WHERE title=?", (title,)))
 
-
 init_database()
-update_book("Bonsai Bug Battle Royale", "Peter Pan Deluxe", 2002, 123456789)
+remove_book(123456789)
