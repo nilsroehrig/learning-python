f0316eea1973106bcb553f3a3cf9d85803905c90
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-12-02 23:04:11 +0100

    add update operation

--- pmc-app05-bookstore/src/book_operations.py
+++ pmc-app05-bookstore/src/book_operations.py
@@ -14,7 +14,7 @@ def init_database():
         'CREATE TABLE IF NOT EXISTS books (title TEXT, author TEXT, year INTEGER, isbn INTEGER)'))
 
 
-def create_entry(title, author, year, isbn):
+def create_book(title, author, year, isbn):
     book_attributes = (title, author, year, isbn)
     if any(val is None for val in book_attributes):
         raise Exception("All book attributes must be set!")
@@ -22,8 +22,17 @@ def create_entry(title, author, year, isbn):
     return with_db(lambda cur: cur.execute('INSERT INTO books VALUES (?, ?, ?, ?)', book_attributes))
 
 
+def update_book(title, author, year, isbn):
+    book_attributes = (title, author, year, isbn)
+    if any(val is None for val in book_attributes):
+        raise Exception("All book attributes must be set!")
+
+    return with_db(lambda cur: cur.execute("UPDATE books SET title=?, author=?, year=? WHERE isbn=?", book_attributes))
+
+
 # def get_by_title(title):
 #     return with_db(lambda cur: cur.execute("SELECT * FROM books WHERE title=?", (title,)))
 
 
 init_database()
+update_book("Bonsai Bug Battle Royale", "Peter Pan Deluxe", 2002, 123456789)
